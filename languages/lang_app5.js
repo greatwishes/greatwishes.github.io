08fc36acd2ff76b28f82a8e79223725aac952067
greatwishes <greatdaylight@gmail.com>
2025-10-17 13:22:10 +0300

    Update lang_app5.js

--- languages/lang_app5.js
+++ languages/lang_app5.js
@@ -71,4 +71,48 @@ function setLanguage(langCode) {
   currentLang = langCode;
 
   // Header
-  document.querySelector('.header h1').innerHTML
+  document.querySelector('.header h1').innerHTML = lang[langCode].headerTitle;
+  document.querySelector('.header p').innerHTML = lang[langCode].headerSubtitle;
+
+  // App description
+  document.querySelector('.app-description h2').innerHTML = lang[langCode].appTitle;
+  document.querySelector('.app-description p').innerHTML = lang[langCode].appDescription;
+  document.querySelector('.app-description h3').innerHTML = lang[langCode].featuresTitle;
+
+  const featureItems = document.querySelectorAll('.app-description ul li');
+  featureItems.forEach((li, index) => {
+    if (lang[langCode].features[index]) {
+      li.innerHTML = `<strong>${lang[langCode].features[index].split(":")[0]}:</strong>${lang[langCode].features[index].split(":")[1]}`;
+    }
+  });
+
+  // Buttons
+  document.querySelectorAll('.btn').forEach(btn => {
+    btn.innerHTML = lang[langCode].downloadBtn;
+  });
+
+  // Navigation menu (القائمة العلوية)
+  const navItems = document.querySelectorAll('.nav-links li a');
+  navItems.forEach((el, index) => {
+    if (lang[langCode].nav[index]) {
+      el.innerHTML = lang[langCode].nav[index];
+    }
+  });
+
+  // Navigation links (الروابط السفلية)
+  const navLinks = document.querySelectorAll('.navigation-links a');
+  navLinks.forEach((el, index) => {
+    if (lang[langCode].navLinks[index]) {
+      el.innerHTML = lang[langCode].navLinks[index];
+    }
+  });
+
+  // Footer
+  const footerElements = document.querySelectorAll('[data-lang-key]');
+  footerElements.forEach(el => {
+    const key = el.getAttribute('data-lang-key');
+    if(lang[langCode].footer[key]){
+      el.innerHTML = lang[langCode].footer[key];
+    }
+  });
+  }
